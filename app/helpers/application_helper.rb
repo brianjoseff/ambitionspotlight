9912755236c27dc1e821bf7ab1b19757baf65b78
tibet000000 <Brian.D.P.Joseff@dartmouth.edu>
2014-09-27 19:20:21 -0700

    redesign

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  
+  def truncate(words, limit)
+    if words.nil?
+      return
+    end
+    string_arr = words.split(' ')
+    string_arr.count > limit ? "#{string_arr[0..(limit-1)].join(' ')}..." : words
+  end
 end
