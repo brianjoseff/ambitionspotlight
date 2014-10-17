78518a2526776b58f50ae2261e3602ff1cb1e9d2
tibet000000 <Brian.D.P.Joseff@dartmouth.edu>
2014-10-16 19:44:29 -0700

    add dafult url to actionmalier

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,7 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
   
-  
+  config.action_mailer.default_url_options = { :host => 'ambitionspotlight.com' }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default :charset => "utf-8"
