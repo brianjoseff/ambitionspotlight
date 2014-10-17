4762624d831d7900be3150d66a4933acf38905ee
tibet000000 <Brian.D.P.Joseff@dartmouth.edu>
2014-10-16 23:32:05 -0700

    default url

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module Ambitionspotlight
   class Application < Rails::Application
     config.serve_static_assets = true
     config.assets.compile = true
+    config.action_mailer.default_url_options = { :host => 'ambitionspotlight.com' }
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
