83f98604b814ad1bfddb7acdcddb435f3468bd06
tibet000000 <Brian.D.P.Joseff@dartmouth.edu>
2014-10-17 00:20:08 -0700

    email fix 2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,8 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
   
-  config.action_mailer.default_url_options = { :host => 'ambitionspotlight.com' }
+  config.action_mailer.default_url_options = { :host => 'www.ambitionspotlight.com' }
+  config.acion_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default :charset => "utf-8"
