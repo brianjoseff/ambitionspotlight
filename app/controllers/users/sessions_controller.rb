fe8324f74059291ee7da6a4222a76648676fa6f2
Brian Joseff <briandpjoseff@gmail.com>
2015-04-13 21:14:02 -0700

    session remember me for 2 years

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -2,7 +2,11 @@ class Users::SessionsController < Devise::SessionsController
 
 
   
- 
+  def create
+    params[:user].merge!(remember_me: 1)
+    super
+  end
+  
   def after_sign_in_path_for(resource)
     user_path(current_user)
   end
