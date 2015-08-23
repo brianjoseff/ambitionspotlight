effe0cafd194b3dcb9b0e6a83db8a08f89fdd624
Brian Joseff <briandpjoseff@gmail.com>
2015-08-23 01:09:49 -0700

    add sign up form to landing page...needs styling still

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,2 +1,18 @@
 module PagesHelper
+    # Methods to allow devise form to work in a non-Devise controller:
+  def resource_name
+    :user
+  end
+
+  def resource
+    @resource ||= User.new
+  end
+
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
+  end
+
+  def resource_class
+    devise_mapping.to
+  end
 end
