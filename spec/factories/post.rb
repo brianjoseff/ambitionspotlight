f173d4328544c682df443364232b5d1d5da2326f
Victor Velazquez <victor.velazquez@crowdint.com>
2015-09-01 00:03:30 -0500

    Remove unused lines from factory

--- spec/factories/post.rb
+++ spec/factories/post.rb
@@ -2,10 +2,3 @@ FactoryGirl.define do
   factory :post do
   end
 end
-
-def linkify_mentions_hashtags_bangbangs(text)
-  text.gsub!(/@([\w]+)(\W)?/, '<a href="/\1" class="pretty-link mention-pretty-link">@\1</a>\2')
-  text.gsub!(/#([\w]+)(\W)?/, '<a href="/search?q=%23\1" class="pretty-link tag-pretty-link">#\1</a>\2')
-  text.gsub!(/!!([\w]+)(\W)?/, '<a href="/search?q=%21%21\1" class="pretty-link bangbang-pretty-link">!!\1</a>\2')
-  text
-end
